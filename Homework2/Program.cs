5ff44cdf4adc9945f9d6f8d6aecb99bbc75d3b51
NarminSH <nshivakhanova@gmail.com>
2022-08-25 13:12:38 +0400

    all ready

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -7,35 +7,35 @@ namespace Homework2
         static void Main(string[] args)
         {
             //2nd task
-            //Console.WriteLine("2nd task");
-            //Console.Write("Type something: ");
-            //string input = Console.ReadLine();
-            //string reverseString = "";
-            //for(int i = input.Length -1 ; i>=0; i--)
-            //{
-            //    reverseString += input[i];
-            //}
-            //Console.WriteLine("Reversed string is: " + reverseString);
+            Console.WriteLine("2nd task");
+            Console.Write("Type something: ");
+            string input = Console.ReadLine();
+            string reverseString = "";
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                reverseString += input[i];
+            }
+            Console.WriteLine("Reversed string is: " + reverseString);
             ////3rd task 
-            //Console.WriteLine("3rd task");
-            //int[] numbers = { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
-            //for (int i = 0; i < numbers.Length; i++)
-            //{
-            //    if(numbers[i] % 2 == 0 || numbers[i] % 3 == 0)
-            //    {
-            //        Console.WriteLine(numbers[i] + " is an even number");
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine(numbers[i] + " is a prime number");
-            //    }
-            //}
+            Console.WriteLine("3rd task");
+            int[] numbers = { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0 || numbers[i] % 3 == 0)
+                {
+                    Console.WriteLine(numbers[i] + " is an even number");
+                }
+                else
+                {
+                    Console.WriteLine(numbers[i] + " is a prime number");
+                }
+            }
             ////1st task 
-            //Console.WriteLine("1st task");
-            //for (int i = 0; i < 100; i++)
-            //{
-            //    Console.WriteLine($"positive = {i} and negative = {-i}" );
-            //}
+            Console.WriteLine("1st task");
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine($"positive = {i} and negative = {-i}");
+            }
 
 
             //4th task 
@@ -43,6 +43,7 @@ namespace Homework2
             bool flag = true;
             int[] inputNumbers = new int[0];
             int increment = 0;
+
 
             do
             {
@@ -66,6 +67,21 @@ namespace Homework2
                             Array.Resize(ref tekEdedler, tekEdedler.Length + 1);
                         }
                     }
+                    int min = tekEdedler[0];
+                    int max = 0;
+                    for (int i = 0; i < tekEdedler.Length; i++)
+                    {
+                        if (tekEdedler[i] < min)
+                        {
+                            min = tekEdedler[i];
+                        }
+                        else if (tekEdedler[i] > max)
+                        {
+                            max = tekEdedler[i];
+                        };
+
+                    }
+                    Console.WriteLine("Final result is " + (max - min));
                 }
 
             } while (flag);
